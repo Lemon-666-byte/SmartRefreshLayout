b3e44d9d0f54e5742f37e87fd349ee17eddfc057
yangjh <13366006925@163.com>
2018-05-26 01:23:03 +0800

    刷新组件

--- app/src/main/java/com/yang/MainActivity.java
+++ app/src/main/java/com/yang/MainActivity.java
@@ -1,17 +1,38 @@
 package com.yang;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.refreshview.SmartRefreshLayout;
+import com.refreshview.api.RefreshLayout;
+import com.refreshview.listener.OnRefreshLoadMoreListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmartRefreshLayout smartRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("tag","fdfsd");
+        smartRefreshLayout = findViewById(R.id.refreshLayout);
+        smartRefreshLayout.autoRefresh();
+//        smartRefreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+//            @Override
+//            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+//
+//            }
+//
+//            @Override
+//            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+//
+//            }
+//        });
+
+        Log.e("tag", "fdfsd");
 
     }
 }
